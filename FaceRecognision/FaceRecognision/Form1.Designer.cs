779df800914d20686780e30700607bcd65b799a6
W0RT4 <kstaniszewski@interia.eu>
2017-02-20 20:21:33 +0100

    Face Recognision Works

--- FaceRecognision/FaceRecognision/Form1.Designer.cs
+++ FaceRecognision/FaceRecognision/Form1.Designer.cs
@@ -29,11 +29,56 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            this.imgCamUser = new Emgu.CV.UI.ImageBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.imgCamUser)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // imgCamUser
+            // 
+            this.imgCamUser.Location = new System.Drawing.Point(12, 70);
+            this.imgCamUser.Name = "imgCamUser";
+            this.imgCamUser.Size = new System.Drawing.Size(565, 382);
+            this.imgCamUser.TabIndex = 2;
+            this.imgCamUser.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(0, 0);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(250, 30);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "START";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.StartRecognision);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(615, 482);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.imgCamUser);
+            this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.imgCamUser)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        public  Emgu.CV.UI.ImageBox imgCamUser;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
     }
 }
 
--- FaceRecognision/FaceRecognision/Form1.cs
+++ FaceRecognision/FaceRecognision/Form1.cs
@@ -7,14 +7,85 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Emgu.CV;
+using Emgu.Util;
+using Emgu.CV.Structure;
+using Emgu.CV.CvEnum;
+using System.Threading;
 
 namespace FaceRecognision
 {
     public partial class Form1 : Form
     {
+
+        private Capture _cap;
+        private CascadeClassifier _cascadeClassifier;
+
         public Form1()
         {
             InitializeComponent();
+
+
+        }
+
+        private void StartRecognision(object sender, EventArgs e)
+        {
+
+            _cap = new Capture();
+
+            Task.Factory.StartNew(BackgroundWorkerDoWork, null);
+            Thread.Sleep(0);
+        }
+        void BackgroundWorkerDoWork(object args)
+        {
+            while (true)
+            {
+                this.BeginInvoke(new MethodInvoker(delegate
+                {
+                    /*imgCamUser.Image = _cap.QueryFrame();
+                    this.InvokeIfRequired(() => imgCamUser.Image = _cap.QueryFrame());
+                   // Console.Write("Pracuje");*/
+
+
+                    _cascadeClassifier = new CascadeClassifier(Application.StartupPath + "\\haarcascade_frontalface_default.xml");
+                    using (var imageFrame = _cap.QueryFrame().ToImage<Bgr, Byte>())
+                    {
+                        if (imageFrame != null)
+                        {
+                            var grayframe = imageFrame.Convert<Gray, byte>();
+                            var faces = _cascadeClassifier.DetectMultiScale(grayframe, 1.1, 10, Size.Empty); //the actual face detection happens here
+                            foreach (var face in faces)
+                            {
+                                imageFrame.Draw(face, new Bgr(Color.BurlyWood), 3); //the detected face(s) is highlighted here using a box that is drawn around it/them
+
+                            }
+                        }
+                        imgCamUser.Image = imageFrame;
+                    }
+                })
+                );
+                Thread.Sleep(300);
+            }
+        }
+
+
+    }
+    public static class ControlExtensions
+    {
+        public static void InvokeIfRequired(this Control control, Action action)
+        {
+            if (control.InvokeRequired)
+                control.Invoke(action);
+            else
+                action();
+        }
+        public static void InvokeIfRequired<T>(this Control control, Action<T> action, T parameter)
+        {
+            if (control.InvokeRequired)
+                control.Invoke(action, parameter);
+            else
+                action(parameter);
         }
     }
+
 }
